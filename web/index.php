8dc7cbcd81f53e034f45140e7f24b9a8897e46ff
Thomas Göttgens <tgoettgens@gmail.com>
2022-11-09 16:07:27 +0100

    display release level and include 2.0 releases

--- web/index.php
+++ web/index.php
@@ -44,14 +44,14 @@ $releases = $client->api('repo')->releases()->all('meshtastic', 'firmware');
 
 foreach ($releases as $release){
     foreach($release['assets'] as $asset){
-        if (strpos($asset['browser_download_url'], '/firmware-1.3')){
+        if (strpos($asset['browser_download_url'], '/firmware-1.3') || strpos($asset['browser_download_url'], '/firmware-2')){
             $display[$release['tag_name']] = $release;
             $versions[$release['tag_name']] = $release['tag_name'];
             if (!file_exists(__DIR__  . '/firmware/' . $asset['name'])){
                 file_put_contents(__DIR__  . '/firmware/' . $asset['name'], file_get_contents($asset['browser_download_url']));
             }
             if (!is_dir(__DIR__  . '/firmware/' . $release['tag_name'])) {
-                exec('unzip ' . __DIR__  . '/firmware/' . $asset['name'].' -d ' . __DIR__  . '/firmware/' . $release['tag_name']);
+                exec('unzip ' . __DIR__  . '/firmware/' . $asset['name'] . ' -d ' . __DIR__  . '/firmware/' . $release['tag_name']);
             }
         }
     }
@@ -60,7 +60,7 @@ foreach ($releases as $release){
 rsort($versions);
 
 foreach ($versions as $version){
-    $ver .= '<option value="'  .$version . '" /> ' . ucfirst($version) . '</option>';
+    $ver .= '<option value="' . $version . '" /> ' . ucfirst($version) . (($display[$version]['prerelease'] == 1) ? " alpha" : " beta") . '</option>';
 }
 
 $template=strtr(file_get_contents('index.tpl.html'), array('###radios###'=>$radios, '###versions###'=>$ver));
